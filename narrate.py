ddb846387baee4cda07b6ccd63ea248f55ab72bd
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-02-03 16:27:08 -0700

    Fixed Tabs

--- narrate.py
+++ narrate.py
@@ -1,16 +1,16 @@
 def getString(addr: int) -> str:
-    # open the file
-    file = open("narration.txt", "r")
-    # make address strings to check against
-    addrStart = f"%{addr}%\n"
-    addrEnd = f"%{addr + 1}%\n"
-    # get the text
-    text = ""
-    isReading = False
-    for line in file:
-        if isReading:
-            if line == addrEnd: break
-            else: text += line
-        if line == addrStart: isReading = True
-    file.close()
-    return text.strip()
+	# open the file
+	file = open("narration.txt", "r")
+	# make address strings to check against
+	addrStart = f"%{addr}%\n"
+	addrEnd = f"%{addr + 1}%\n"
+	# get the text
+	text = ""
+	isReading = False
+	for line in file:
+		if isReading:
+			if line == addrEnd: break
+			else: text += line
+		if line == addrStart: isReading = True
+	file.close()
+	return text.strip()
