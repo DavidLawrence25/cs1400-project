edf459cea989e5bcd6138e000d1ccc4c4588d098
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-03-23 13:16:01 -0600

    Basic Interface File

---
+++ interface.py
@@ -0,0 +1,14 @@
+class Event(object):
+	def __init__(self) -> None:
+		self.__event_handlers = []
+	
+	def __iadd__(self, handler):
+		self.__event_handlers.append(handler)
+		return self
+	
+	def __isub__(self, handler):
+		self.__event_handlers.remove(handler)
+		return self
+	
+	def __call__(self, *args, **kwargs) -> None:
+		for handler in self.__event_handlers: handler(*args, **kwargs)
